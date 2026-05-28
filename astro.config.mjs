7bc3e9628ffa700233e0591089e37a1cd86f2bb5
teffidev <tefyberal09@gmail.com>
2026-05-28 14:04:19 -0500

    Deploy with vercel & Neon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   output: "server",
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 
   vite: {
     plugins: [tailwindcss()],
